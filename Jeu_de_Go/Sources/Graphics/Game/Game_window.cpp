247a981918010ec6d60d754439b4d91a29a92084
Unknown <julien.ro34@gmail.com>
2017-04-25 22:19:30 +0200

    a
    a

--- Jeu_de_Go/Sources/Graphics/Game/Game_window.cpp
+++ Jeu_de_Go/Sources/Graphics/Game/Game_window.cpp
@@ -161,7 +161,7 @@ Screens Game_window::keyPressed(const sf::Event::KeyEvent & key)
 				Goban gob = board.getGoban();
 				Arbre abr(gob, Etat::BLANC);
 				//abr.Tsumego(board.getGoban().coord(1, 2));  //Erreur de free
-				std::cout<<IA::Tsumego2(abr, board.getGoban().coord(0, 1))<<std::endl;
+				IA::Tsumego(abr, board.getGoban().coord(0, 1));
 			}
 			else
 			{
